df0bc9f14cc9a91b99082b1211163dc2593187a6
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-28 20:44:45 -0300

    RC-33. 06. UDT&O. Branch with if expressions & scopes

--- src/Main.kt
+++ src/Main.kt
@@ -3,30 +3,38 @@
 * Programming in Kotlin. Fundamentals
 * Part 01. Use datatype & operations
 *
-*  5. Combine logical operators
+*  6. Combine logical operators
 */
 
-fun main(arguments: Array<String>) {
-    val myAge = 12
-    val country = "USA"
-
-    val isLegalDriving =
-                (country == "Croatia" && myAge >= 18)
-                || (country == "USA" && myAge >= 16)
-    println(isLegalDriving)
-
-    if (isLegalDriving) {
-        println("You can drive")
-    } else  {
-        println("You've got a bit more time to got")
-    }
-
-    if (myAge >= 18) println("I'm an adult") else println("I'm still underaged!")
-
-    val ageMessage = if (myAge >= 18) "I'm an adult" else "I'm still underaged!"
-    println(ageMessage)
+fun main() {
+    val myAge = 23
 
     println(if (myAge >= 18) "I'm an adult" else "I'm still underaged!")
+
+    if (myAge >= 18) {
+        val country = "Croatia"
+
+        if (country == "USA") {
+            println("You're allowed to drive, but not to drink!")
+
+        } else if (country == "Croatia") {
+            println("You're allowed to both drink and drive (not at the same time!)")
+        }
+    }
+
+    // country here is not in scope
+
+    val yearOfBirth = 1996
+
+    val generationCohort = if(yearOfBirth > 1964 && yearOfBirth < 1980) {
+        "Generation X"
+    } else if (yearOfBirth > 1979 && yearOfBirth < 1995) {
+        "Millenial"
+    } else {
+        "Generation Z"
+    }
+
+    println(generationCohort)
 }
 
 
