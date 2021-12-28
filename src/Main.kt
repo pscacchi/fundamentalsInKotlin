7864a3eca967952bffd4bd3edccf78d3578f7ee4
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-28 12:57:17 -0300

    RC-28. 01. UDT&O. Introduction

---
+++ src/Main.kt
@@ -0,0 +1,42 @@
+/*
+Programming in Kotlin. Fundamentals
+Part 01. Use datatype & operations
+
+01. Introduction
+
+
+- val vs var
+- data type
+- converting & comparing values
+- Branching with fi statements
+- declaring function
+
+val => to declare a constant
+var => to declare a constant
+
+Data type:
+- Int: integer
+- String: serie of characters
+
+Branching Code
+if (programmingIsAwesome) {
+    println("Having loads of fun")
+} else {
+    println("Do something else!"
+}
+
+Deflaring & using functions
+fun main(arguments: Array<String>){}
+fun printName(name: String) {
+
+}
+
+
+printName("Filip")
+
+*/
+
+fun main() {
+
+
+}
