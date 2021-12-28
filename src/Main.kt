c0405931d2013b685c3bd07b4a0ec86085af8901
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-28 14:38:38 -0300

    RC-29. 02. UDT&O. Set up IntelliJ IDEA

--- src/Main.kt
+++ src/Main.kt
@@ -1,42 +1,62 @@
+
+/**
+ * This is a documentation-style comment
+ * You can reference things here, like [main] function.
+ * Yoy can also reference parameters, like the [arguments]
+ *
+ * @param arguments
+ * @author Filip
+ *
+ * */
+
+
+fun main(arguments: Array<String>) {
+    var myAge: Int = 23
+    val name: String = "Filip"
+    val lastName = "Babic"
+    val weight = 63.5
+
+    println(name)
+
+    println("I'm $myAge years old")
+
+    myAge += 1
+    println("I'm $myAge years old")
+
+    println("$name $lastName is $myAge years old, and weighta $weight ks")
+
+    val fullName = "$lastName, $name"
+    println(fullName)
+
+    val ageAsString = myAge.toString()
+    println(ageAsString)
+
+    val ageFromString = "35".toInt()
+    println(ageFromString)
+
+    val nameLength = fullName.length
+    println(nameLength)
+
+    // This is line comment
+
+    /*
+    * This a multiline comment.
+    * You don't have to add // before each line
+    *
+    *
+     */
+}
+
+
 /*
 Programming in Kotlin. Fundamentals
 Part 01. Use datatype & operations
 
-01. Introduction
+2. Set-Up IntelliJ IDEA
 
-
-- val vs var
-- data type
-- converting & comparing values
-- Branching with fi statements
-- declaring function
-
-val => to declare a constant
-var => to declare a constant
-
-Data type:
-- Int: integer
-- String: serie of characters
-
-Branching Code
-if (programmingIsAwesome) {
-    println("Having loads of fun")
-} else {
-    println("Do something else!"
-}
-
-Deflaring & using functions
-fun main(arguments: Array<String>){}
-fun printName(name: String) {
-
-}
-
-
-printName("Filip")
+- DownloadIntelliJ IDEA (Community version)
+- Use default setting
+- Create main.kt file on source directory (src)
+- All Kotlin program need a main function.
 
 */
-
-fun main() {
-
-
-}
