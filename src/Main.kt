91b1c366127e19a3ad4ef4c8a41e8cac9b0c31c5
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-29 15:28:32 -0300

    RC-38. 11. STIC. Introduction

--- src/Main.kt
+++ src/Main.kt
@@ -1,77 +1,41 @@
 
 /*
 * Programming in Kotlin. Fundamentals
-* Part 01. Use datatype & operations
+* Part 02. Store Data In Collections
 *
-*  10. Conclusion
+*  11. Introduction
 */
 
 /*
-* val name = "Filip"
-*    var age = 23
+* Part 2. Concepts
 *
-*    val ageAsString: String = age.toString()
+* - Pairs & triples
+*   + Groups of two to three pieces of data
+*   + Can represent classes or structured data
+* - Collections
+*   + Single construct which can hold multiple values of the same type
+*   + Can work with any type
+*   + Can be iterated over
+*   + type of collections
+*     * Array
+*     * List
+*     * Set (no duplicated)
+*     * Map (a dictionary)
+*   + Generics<?>
+*     * Classes & functions operating on yet undefined types
+*     * Types are determined when creating objects
+*     * Generics examples
+*          public fun <T>List(size: Int): List<T>
+*          class Container<T>(val value: T)
+*          val namesList = List<String>(10)
+* - Mutability
 *
-*   val nameLongerThanAge = name.length > ageAsString.length
-*
-*   if (nameLongerThanAge) {
-*
-*   }
-*
-*   val firstName = "Filip"
-*   val lastName = "Babic"
-*
-*   val isNameValid =
-*               firstName.isNotEmpty() &&
-*               lastName.isNotEmpty()
-*
-*   val message =
-*       if (isNameValid) "Your full name is $lastName, $firstName"
-*       else "Your name seems to be invalid"
-*
-*   val nickname: String? = null
-*   if (nickname != null) {
-*       // smart cast
-*   }
-*
-*   val myNickname = nickname ?: "Filip"
-*   val nicknameLength = nickname?.length ?: myNickname.length
+* - Collection performance
 * */
-
-/*
-* Challenge.
-*
-* Create several values describing you
-* as a person, in piece of data. The
-* pieces of data are as follows:
-* - Name
-* - Last name
-* - Nickname (if exists)
-* - Country
-* - Age
-*
-* Then find your year of birth using
-* your age, and print out your data in
-* the format included in the project,
-* while following the extra rules
-* within the project
-*  */
 
 fun main() {
 
-    val name = "Pablo"
-    val lastName = "Sca"
-    val nickname: String? = null
-    val country = "Argentina"
-    val age = 45
 
-    val nameToPrint = if (age >= 18) "$name," else ""
-    val nicknameToPrint = nickname ?: ""
-    val yearOfBirth = 2022 - age
-
-    val userData = "$nameToPrint $nicknameToPrint $lastName, born on $yearOfBirth, in $country"
-
-    println(userData)
 
 }
 
