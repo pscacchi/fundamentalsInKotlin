7e49e24e2a23b890b8e8553e9ed0fc23e705b0d8
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-29 18:47:01 -0300

    RC-42. 15. STIC. Create lists of data

--- src/Main.kt
+++ src/Main.kt
@@ -3,86 +3,52 @@
 * Programming in Kotlin. Fundamentals
 * Part 02. Store Data In Collections
 *
-*  13. Allocate arrays
+*  15. Create lists of data
 */
 
 /*
-* Difference in collections
-*
-* - Memory management
-* - Ability to change (Multability)
-* - Performance
-*   - Accessing data
-*   - Adding / removing data
-*   - Iterating over values
-* */
-
-/*
-* ARRAYS
-*
-* Connected group of values of the same type
-* It's like a fixed or a glued series of data of same type, but
-* each each data value can be different.
-* It's a static collection, because when you create an array,
-* you have to allocate the fixed of static chunk o memory. The size
-* can not change. (no remove or add)
-*
-* Big O notation:
-*    - instant -> accessing values
-*    - linear operation -> grows slower with more elements
-*
+* powered by Array
+* Index data access - O(1)
+* Iteration - O(n)
+* Dynamic in size: Add [O(1) append at end] & remove [O(n) shift the list]
 * */
 
 fun main() {
-//    val cauntry1 = "Russia"
-//    val country2 = "USA"
-//    val country3 = "Poland"
+    val countries = listOf("Russia", "USA", "The Netherlands")
 
-    val visitedCountries = arrayOf<String>(
-        "Russia", "USA", "Poland",
-        "The Netherlands", "Romania", "Greece",
-        "Germany", "Spain", "Portugal",
-        "Serbia", "Hungary", "Austria"
-    )
+    println(countries)
 
-    println(visitedCountries)
-    println(visitedCountries.size)
+    val mutableList = countries.toMutableList()
 
-    println(visitedCountries[3])
+    mutableList.add("Germany")
+    mutableList.add(1, "Greece")
 
-//    println(visitedCountries[20])  // out of bondary
-//    println(visitedCountries[-1])  // out of bondary
+    println(mutableList)
 
-    visitedCountries + "Belgium"
-    println(visitedCountries.size)
-    println((visitedCountries + "Belgium").size)
+    mutableList.addAll(3, listOf("Austria", "Poland", "Hungary"))
+    println(mutableList)
 
-    visitedCountries[1] = "The United States of America"
-    println(visitedCountries[1])
+    val hasBeenToJapan = "Japan" in mutableList
+    println(hasBeenToJapan)
 
-    val sizedArray = Array(10) { "" }
-    val emptyArray = emptyArray<String>()
+    mutableList.remove("Japan")
+    mutableList.removeAt(0)
+    mutableList.removeAll(listOf("Russia", "Japan", "USA", "Germany"))
 
-    val arrayOfInts = intArrayOf(2, 3, 4, 5, 6, 7)
-    println(arrayOfInts)
+    println(mutableList)
 
-    val intArray = IntArray(10)
-    println(intArray)
+    mutableList[2] = "Croatia"
+    val combinedList = countries + mutableList
+    val emptyList = emptyList<String>()
 
-    println(visitedCountries.lastIndex)
-    println(visitedCountries.last())
-    println(visitedCountries.first())
+    println(mutableList)
+    println(combinedList)
+    println(emptyList)
 
-    val currentCountry = "Spain"
-    println(visitedCountries.contains(currentCountry))
-
-    val hasVisitedSpain = currentCountry in visitedCountries
-    val hasNotVisitedSpain = currentCountry !in visitedCountries
-
-    println(hasVisitedSpain)
-    println(hasNotVisitedSpain)
-
-
+    mutableList.clear()
+    println(mutableList)
 }
+
+
 
 
