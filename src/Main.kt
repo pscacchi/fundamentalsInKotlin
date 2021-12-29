81280c7cab2d4b198fd56a04589106c722063bbe
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-29 17:50:05 -0300

    RC-41. 12. STIC. Allocate arrays

--- src/Main.kt
+++ src/Main.kt
@@ -3,41 +3,84 @@
 * Programming in Kotlin. Fundamentals
 * Part 02. Store Data In Collections
 *
-*  13. Challenger: Pair & triples
+*  13. Allocate arrays
 */
 
 /*
-* Create a Pair which hold
-* information about a Bank Account
-* A Bank Account needs to have a
-* Credit Card connected to it, and a
-* balance. Hint: you can nest a Triple
-* in a Pair
+* Difference in collections
 *
-* A Credit Card should have a Card
-* number, security code, and the
-* type of card(e.g. "Visa", "MasterCard").
-*
-* Print out the data for the Account,
-* While hiding the security code.
+* - Memory management
+* - Ability to change (Multability)
+* - Performance
+*   - Accessing data
+*   - Adding / removing data
+*   - Iterating over values
 * */
+
+/*
+* ARRAYS
+*
+* Connected group of values of the same type
+* It's like a fixed or a glued series of data of same type, but
+* each each data value can be different.
+* It's a static collection, because when you create an array,
+* you have to allocate the fixed of static chunk o memory. The size
+* can not change. (no remove or add)
+*
+* Big O notation:
+*    - instant -> accessing values
+*    - linear operation -> grows slower with more elements
+*
+* */
+
 fun main() {
-    val firstName = "Pablo"
-    val lastName = "Sca"
-    val person = Pair(firstName, lastName)
+//    val cauntry1 = "Russia"
+//    val country2 = "USA"
+//    val country3 = "Poland"
 
-    val creditNumber = 12345678901234
-    val securityCode = 1234
-    val typeCard = "Visa"
+    val visitedCountries = arrayOf<String>(
+        "Russia", "USA", "Poland",
+        "The Netherlands", "Romania", "Greece",
+        "Germany", "Spain", "Portugal",
+        "Serbia", "Hungary", "Austria"
+    )
 
-    val creditCard = Triple(creditNumber, securityCode, typeCard)
+    println(visitedCountries)
+    println(visitedCountries.size)
 
-    val bankAccount = Pair(person, creditCard)
+    println(visitedCountries[3])
 
-    println("First Name: ${bankAccount.first.first}")
-    println("Last Name: ${bankAccount.first.second}")
-    println("Credit Number: ${bankAccount.second.first}")
-    println("Type of CreditCard: ${bankAccount.second.third}")
+//    println(visitedCountries[20])  // out of bondary
+//    println(visitedCountries[-1])  // out of bondary
+
+    visitedCountries + "Belgium"
+    println(visitedCountries.size)
+    println((visitedCountries + "Belgium").size)
+
+    visitedCountries[1] = "The United States of America"
+    println(visitedCountries[1])
+
+    val sizedArray = Array(10) { "" }
+    val emptyArray = emptyArray<String>()
+
+    val arrayOfInts = intArrayOf(2, 3, 4, 5, 6, 7)
+    println(arrayOfInts)
+
+    val intArray = IntArray(10)
+    println(intArray)
+
+    println(visitedCountries.lastIndex)
+    println(visitedCountries.last())
+    println(visitedCountries.first())
+
+    val currentCountry = "Spain"
+    println(visitedCountries.contains(currentCountry))
+
+    val hasVisitedSpain = currentCountry in visitedCountries
+    val hasNotVisitedSpain = currentCountry !in visitedCountries
+
+    println(hasVisitedSpain)
+    println(hasNotVisitedSpain)
 
 
 }
