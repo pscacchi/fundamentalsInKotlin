376bef8f91d8512d9410507ca7862506a6a3aaa9
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-28 21:43:25 -0300

    RC-33. 07. UDT&O. Challenge. If expressions & boolean logic

--- src/Main.kt
+++ src/Main.kt
@@ -3,38 +3,40 @@
 * Programming in Kotlin. Fundamentals
 * Part 01. Use datatype & operations
 *
-*  6. Combine logical operators
+*  7. Challenge. If expressions & bollean logic
+*/
+
+/*
+* Check if the user data for registering an account is valid. Data is valid if
+*
+* 1. Properties are not empty
+* 2. Username has at least 6 characters.
+* 3. Password has at least 10 characters.
+* 4. The email contains a '@' and a '.'.
+*
+* Hint.: use the 'contains()' function on a String
+*
+* Use 'if' as an expression, to assign a respective
+* error message if any of the cases fail! Then print
+* it out.
+*
+* The messages are prepared for you in the project.
 */
 
 fun main() {
-    val myAge = 23
+    val username = "Pablos"
+    val password = "12345678"
+    val email = "pablos@endava.com"
 
-    println(if (myAge >= 18) "I'm an adult" else "I'm still underaged!")
+    val message =
+        if (username.isEmpty() || password.isEmpty() || email.isEmpty()) "You must fill all fields"
+        else if (username.length >= 6) "Username is too short"
+        else if (password.length >= 10) "Password is too short"
+        else if (email.contains("Q") && email.contains(".")) "Email doesn't contains a '@' or a '.'"
+        else "Succesfull registration!"
 
-    if (myAge >= 18) {
-        val country = "Croatia"
+    print(message)
 
-        if (country == "USA") {
-            println("You're allowed to drive, but not to drink!")
-
-        } else if (country == "Croatia") {
-            println("You're allowed to both drink and drive (not at the same time!)")
-        }
-    }
-
-    // country here is not in scope
-
-    val yearOfBirth = 1996
-
-    val generationCohort = if(yearOfBirth > 1964 && yearOfBirth < 1980) {
-        "Generation X"
-    } else if (yearOfBirth > 1979 && yearOfBirth < 1995) {
-        "Millenial"
-    } else {
-        "Generation Z"
-    }
-
-    println(generationCohort)
 }
 
 
