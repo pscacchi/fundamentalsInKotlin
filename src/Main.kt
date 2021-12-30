2173a4d076d6629f3dfe5bc66b9912fda5394c70
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-30 10:17:07 -0300

    RC-61. 34. EMC. Conclusion

--- src/Main.kt
+++ src/Main.kt
@@ -3,36 +3,19 @@
 * Programming in Kotlin. Fundamentals
 * Part 02. Store Data In Collections
 *
-*  33. Challenge. Set
+*  34. Conclusion
 */
 
 /*
-* CHALLENGE
+* NEW CONCEPTS
 *
-* - Create a list of string, describing items
-* on your table. If items appear more than once,
-* add them multiple times, like two pens.
-*
-* - Turn that list into a set, and check if the
-* duplicate appear
-*
-* -Remove an item you don't really need on your desk,
-* e.g. car keys
-*
-* - Iterate over the items, and print them out
-*
+* Maps
+* Map entries
+* Sets & unique values
 * */
 
 fun main() {
-    val things = listOf("cable", "cable", "pendrive", "pendrive", "phone", "antenna", "batery")
 
-    val thingSet = things.toMutableSet()
-
-    println(thingSet.remove("batery"))
-
-    for (item in thingSet) {
-        println(item)
-    }
 }
 
 
