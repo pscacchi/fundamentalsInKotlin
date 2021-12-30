b6d169e345c19323cec74833c13cd3b43a7ca38b
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-29 21:28:15 -0300

    RC-45. 18. MCF. Introduction

--- src/Main.kt
+++ src/Main.kt
@@ -3,13 +3,28 @@
 * Programming in Kotlin. Fundamentals
 * Part 02. Store Data In Collections
 *
-*  17. Conclusion
+*  18. Introduction
 */
 
 /*
 * CORE CONCEPTS
 *
-* A lots
+* Val vs var
+* Data types (Int, String, boolean, nullable types)
+* Comparison && logical operators, converting values
+* Branching with if statements
+* Declaring functions
+* If/else expressions & scope
+* Collections
+* Big O notation & performance
+*
+* CONCEPTS
+*
+* Control flow
+* Range
+* Looping code
+* Iterating over collections
+* When expression
 * */
 
 fun main() {
