61b154f3011e0596aae05a111c7584620480e8bd
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-30 20:55:53 -0300

    RC-67. 40. WF. Conclusion.

--- src/Main.kt
+++ src/Main.kt
@@ -2,73 +2,33 @@
 * Programming in Kotlin. Fundamentals
 * Part 05. Write functions
 *
-*  39. Declare & pass functions
+*  40. Conclusion
 */
 
 /*
-* FUNCTIONS AS TYPE
+* NEW CONCEPTS
 *
-* Functions are type too
-* Store functions in variables
-* Expect them as parameters in other
-*   functions
+* Function syntax
+* Function parameters
+* Returning values
+* Overloading functions
+* Functions as parameters
 *
-* FUNCTION SINTAX
+* FUNCTIONS FUNCTIONALITY
 *
-* fun functionName(parameters): ReturnType {
-*   body
+* Lambdas: (Int)->Boolean
+* Default parameters
+* Fun printRange(range: IntRange = 0..10) {
+*   ...
 * }
-*
-* BASE FUNCTION SIGNATURE
-* (parameter)->ReturnType
-*
-* LAMBDA EXAMPLE
-*
-* (Int)->Boolean
+* Named arguments:
+* printRange(range = 5..15)
 * */
 
 fun validatePassword(password: String) = password.length >= 10
 
 fun main()  {
-    val email: String? = "email@mail.com"
-    val password: String? = "password"
 
-    fun validateString(input: String?, inputType: String) =
-        if (input == null || input.isBlank()) {
-            false
-        } else if (inputType == "Password") {
-            input.length >= 10
-        } else if (inputType == "Email"){
-            input.contains("@")
-        } else {
-            println("Cannot verify this input")
-            false
-        }
-
-    fun validateString(input: String?, validator: (String) -> Boolean) =
-        if (input == null || input.isBlank()) {
-            false
-        } else {
-            validator(input)
-        }
-
-    validateString("Hello World", "Welcome message")
-
-    val isValidEmail = validateString(email, "Email")
-    println(isValidEmail)
-
-//    val isValidPassword = validateString(password) { input -> input.length >= 10 } // is not a good practice
-    val isValidPassword = validateString(password, ::validatePassword)
-    println(isValidPassword)
-
-    val passwordValidator = ::validatePassword
-    println(passwordValidator)
-
-    val validator: (String?) -> Boolean = { input ->
-        input != null && validatePassword(input)
-    }
-
-    println(validator)
 }
 
 
