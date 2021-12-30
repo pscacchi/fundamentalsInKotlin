e961f38167b0539863b2556eb5e06c9e6db81e15
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-30 08:44:43 -0300

    RC-55. 28. EMC. Introduction

--- src/Main.kt
+++ src/Main.kt
@@ -3,18 +3,30 @@
 * Programming in Kotlin. Fundamentals
 * Part 02. Store Data In Collections
 *
-*  27. MCF. Conclusion
+*  28. Introduction
 */
 
 /*
+* CORE CONCEPTS
+*
+* Using values
+* Creating Collections of Data
+* Iterating over collections
+* Utilize idiomatic Kotlin with expressions & operators
+* Handle nullables
+* Manipulate control flow
+*
 * NEW CONCEPTS
 *
-* Control flow
-* Ranges
-* Looping code
-* Nested loops
-* Iterating over collections
-* Labels When expression
+* Maps
+*   Collections of Pairs
+*   Also known as "Dictionary"
+*   Produce some output, when given certain input
+* Map entries
+*   [Key] -> [Value]
+* Sets & unique values
+*   Similar to Math-based set
+*   Unique elements
 * */
 
 fun main() {
