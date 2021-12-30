dd8701d36ce129e44c0d89ca8adff10cda357b07
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-30 08:35:48 -0300

    RC-54. 27. MCF. Challenge. Conclusion

--- src/Main.kt
+++ src/Main.kt
@@ -3,34 +3,22 @@
 * Programming in Kotlin. Fundamentals
 * Part 02. Store Data In Collections
 *
-*  26. MCF. Challenge. When expressions
+*  27. MCF. Conclusion
 */
 
 /*
-* CHALLENGE
+* NEW CONCEPTS
 *
-* Use a when expression to return
-* which century an arbitrary year is
-* from (as a String)
-*
-* Cover at least the last three
-* centuries, and return "This was
-* looong ago!" for others
-* then prin out the returned value
-*
-* Use Ranges for year comparison
+* Control flow
+* Ranges
+* Looping code
+* Nested loops
+* Iterating over collections
+* Labels When expression
 * */
 
 fun main() {
-    val year = 1976
 
-    val century = when (year) {
-        in 2000..1999 -> "21th century"
-        in 1900..1999 -> "20th century"
-        in 1800..1899 -> "19th century"
-        else -> "This was looong ago!"
-    }
-    println(century)
 }
 
 
