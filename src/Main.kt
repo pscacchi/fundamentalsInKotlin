2bafecf7809800c49d92040bd1eeba2f5d77ef99
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-30 09:27:33 -0300

    RC-57. 30. EMC. Work with maps

--- src/Main.kt
+++ src/Main.kt
@@ -3,18 +3,33 @@
 * Programming in Kotlin. Fundamentals
 * Part 02. Store Data In Collections
 *
-*  29. Create & access maps
+*  30. Work with maps
 */
 
 /*
-* MAP
+* CREATING MAPS
 *
-* Collection of Paris
-* Also known as Dictionary
-* Produce some output, when certain input
+* val users = mapOf("ID13" to "Mark")
+* val empty = emptyMap<String, String>()
+*
+* MUTABILITY
+* Referential mutability
+*   val name = "Filip"
+*   name = "Mark" // nope
+* InternalM mutability
+*   val names = arrayOf("Filip", "Mark")
+*   names[0] = "Phillip"
+* Content mutability
+*   val map = mapOf("Strategy" to
+*        mutableListOf("HoMM3", "Civilization VI")
+*   ) // map, cannot be changed
+*
+* map["Strategy"]?.add("Command & Conquer") // contents can be changed
+*
 * */
 
 fun main() {
+
     val videoGamesCollection = mutableMapOf<String, MutableList<String>>()
     println(videoGamesCollection)
 
@@ -33,6 +48,35 @@ fun main() {
         "content/type" to "application/json"
     )
     println(authenticationHeaders)
+
+    // ---
+
+    videoGamesCollection["Strategy"] = mutableListOf("Heroes of Might & Magic 3")
+    println(videoGamesCollection)
+
+    videoGamesCollection["Strategy"]?.add("Civilization VI")
+    println(videoGamesCollection)
+
+    val removedActionGames = videoGamesCollection.remove("Action")
+    println(removedActionGames)
+    println(videoGamesCollection)
+
+    //authenticationHeaders["Authorization"] = "new api key"
+    videoGamesCollection["Strategy"]?.add("Command & Conquer")
+    videoGamesCollection["Shooter"] = mutableListOf("DOOM")
+
+    for (key in videoGamesCollection.keys) {
+        println(key)
+    }
+
+    for (value in videoGamesCollection.values) {
+        println(value)
+    }
+
+    for ((key, value) in videoGamesCollection) {
+        println("Video games in the $key genre you own are: $value")
+
+    }
 }
 
 
