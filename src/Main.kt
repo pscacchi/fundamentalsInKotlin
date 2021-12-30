7e6f4c87dee532f97d765b59f37048a9a3767f72
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-30 15:04:41 -0300

    RC-66. 38. WF. Challenge: Functions

--- src/Main.kt
+++ src/Main.kt
@@ -3,73 +3,43 @@
 * Programming in Kotlin. Fundamentals
 * Part 05. Write functions
 *
-*  37. Return data from functions
+*  38. Challenge: Functions
 */
 
 /*
-* FUNCTION EXTRAS
+* CHALLENGE 1
 *
-* Functions as expressions
-* Omitting the body
+* Create a function which takes in
+* two parameters - a name and a last name.
 *
-* FUNCTION SYNTAX
+* Because not everyone has a last name, leave
+* the lastName parameter to be a empty String
+* if it is not passed in.
 *
-* fun functionName(Parameters): ReturnType {
-*   boddy
-* }
+* Then return how long the person's full name is.
 *
-* FUNCTION OVERLOAD
+* CHALLENGE 2:
 *
-*fun printRange(range: Int) {
-*  ...
-* }
+* Overload the function from the first challenge,
+* by adding a list of String parameter, for middle
+* names, in case someone has one or more middle names.
 *
-* fun printRange(range: CharRange) {
-*  ---
-* }
+* Use the function to return the full name length,
+* for a name with and without middle names.
+*
+* Remmember to use named arguments if needed
 * */
 
 fun main()  {
-    fun createRange(start: Int, end: Int, isHalfOpen: Boolean = false) =
-        if (isHalfOpen) start until end
-        else start..end
+    fun getFullNameLength(name: String, lastName: String = "") = (name + lastName).length
+    fun getFullNameLength(name: String, middleNames: List<String> = listOf<String>(), lastName: String = "") =
+        (name + lastName).length + middleNames.sumOf { it.length }
 
-    fun createRange(start: Char, end: Char, isHalfOpen: Boolean = false) =
-        if (isHalfOpen) start until end
-        else start..end
+    var myNameLength = getFullNameLength("Pablo", "Sca")
+    println(myNameLength)
 
-    fun printRange(range: IntRange) {
-        for (number in range) print("$number \t")
-        println()
-    }
-    fun printRange(range: CharRange) {
-        for (number in range) print("$number \t")
-        println()
-    }
-
-    val closedRange = createRange(1, 10)
-    printRange(closedRange)
-
-    val halfOpenRange = createRange(1, 10, true)
-    printRange(halfOpenRange)
-
-    val usingNamedArguments = createRange(isHalfOpen = true, start = 10, end = 15)
-    printRange(usingNamedArguments)
-
-    val charRange = createRange('A', 'Z')
-    printRange(charRange)
-
-    fun parseCoordinates(input:String): Pair<Double, Double> {
-        val latitudeLongitud = input.split(",")
-
-        val latitude = latitudeLongitud[0].toDouble()
-        val longitud = latitudeLongitud[1].toDouble()
-
-        return latitude to longitud
-    }
-
-    val (latitude, longitude) = parseCoordinates("45.46,18.68")
-    println("The latitude is: $latitude, and the longitude is: $longitude")
+    val length = getFullNameLength("Salvador", listOf("Domingo", "Felipe", "Jacinto"), "Dali")
+    println(length)
 
 }
 
