57dc1f58951dde7a5f664c2054c61ff4ebf3eb46
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-30 14:40:26 -0300

    RC-63. 37. WF. Return data from function

--- src/Main.kt
+++ src/Main.kt
@@ -3,30 +3,46 @@
 * Programming in Kotlin. Fundamentals
 * Part 05. Write functions
 *
-*  36. Write custom functions
+*  37. Return data from functions
 */
 
 /*
-* FUNCTIONS
+* FUNCTION EXTRAS
 *
-* Separate & reuse code
-* Execute from anywhere
-* Parameters
-* Return type
-* Function body
+* Functions as expressions
+* Omitting the body
 *
 * FUNCTION SYNTAX
 *
-* fun functionName(parameters): ReturnType
-*   body
+* fun functionName(Parameters): ReturnType {
+*   boddy
+* }
+*
+* FUNCTION OVERLOAD
+*
+*fun printRange(range: Int) {
+*  ...
+* }
+*
+* fun printRange(range: CharRange) {
+*  ---
+* }
 * */
 
 fun main()  {
-    fun createRange(start: Int, end: Int, isHalfOpen: Boolean = false): IntRange =
+    fun createRange(start: Int, end: Int, isHalfOpen: Boolean = false) =
+        if (isHalfOpen) start until end
+        else start..end
+
+    fun createRange(start: Char, end: Char, isHalfOpen: Boolean = false) =
         if (isHalfOpen) start until end
         else start..end
 
     fun printRange(range: IntRange) {
+        for (number in range) print("$number \t")
+        println()
+    }
+    fun printRange(range: CharRange) {
         for (number in range) print("$number \t")
         println()
     }
@@ -40,7 +56,21 @@ fun main()  {
     val usingNamedArguments = createRange(isHalfOpen = true, start = 10, end = 15)
     printRange(usingNamedArguments)
 
-    printRange(createRange(3,5, isHalfOpen = true))
+    val charRange = createRange('A', 'Z')
+    printRange(charRange)
+
+    fun parseCoordinates(input:String): Pair<Double, Double> {
+        val latitudeLongitud = input.split(",")
+
+        val latitude = latitudeLongitud[0].toDouble()
+        val longitud = latitudeLongitud[1].toDouble()
+
+        return latitude to longitud
+    }
+
+    val (latitude, longitude) = parseCoordinates("45.46,18.68")
+    println("The latitude is: $latitude, and the longitude is: $longitude")
+
 }
 
 
