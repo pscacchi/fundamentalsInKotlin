3c1bb1c6d49f413ea9fede480b3b73d83d961856
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-30 12:14:06 -0300

    RC-62. 36. WF. Write custom functions

--- src/Main.kt
+++ src/Main.kt
@@ -3,21 +3,44 @@
 * Programming in Kotlin. Fundamentals
 * Part 05. Write functions
 *
-*  35. Introduction
+*  36. Write custom functions
 */
 
 /*
-* NEW CONCEPTS
+* FUNCTIONS
 *
-* Function syntax
-* Function parameters
-* Return values
-* Overloading functions
-* Functions as parameters
+* Separate & reuse code
+* Execute from anywhere
+* Parameters
+* Return type
+* Function body
+*
+* FUNCTION SYNTAX
+*
+* fun functionName(parameters): ReturnType
+*   body
 * */
 
-fun main() {
+fun main()  {
+    fun createRange(start: Int, end: Int, isHalfOpen: Boolean = false): IntRange =
+        if (isHalfOpen) start until end
+        else start..end
 
+    fun printRange(range: IntRange) {
+        for (number in range) print("$number \t")
+        println()
+    }
+
+    val closedRange = createRange(1, 10)
+    printRange(closedRange)
+
+    val halfOpenRange = createRange(1, 10, true)
+    printRange(halfOpenRange)
+
+    val usingNamedArguments = createRange(isHalfOpen = true, start = 10, end = 15)
+    printRange(usingNamedArguments)
+
+    printRange(createRange(3,5, isHalfOpen = true))
 }
 
 
