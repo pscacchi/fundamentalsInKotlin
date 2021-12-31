f785807c64b116add4022c57223af82b1d2153dc
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-30 21:25:59 -0300

    RC-69. 42. CC. Model & create class

--- src/Main.kt
+++ src/Main.kt
@@ -2,32 +2,47 @@
 * Programming in Kotlin. Fundamentals
 * Part 06. Create classes
 *
-*  41. Introduction
+*  42. Model & create class
 */
 
 /*
-* NEW CONCEPTS
-*
-* Classes
-* Custom data types
-* Properties, methods, mutability
-* Modifiers
-*
-* CLASSES
-*
-* Well-defined group of data
-* Always represents the data defined in the class
-* Properties - state of the class
-* Methods - behavior of the class
-* Contructor - a special function of a classes which
-* creates an object of the class.
 *
 *
 * */
 
-fun main()  {
+//class Person {
+//    var name: String = ""
+//    var lastName: String? = null
+//    var age: Int = 0
+//}
 
+class Person(val name: String,
+             val lastName: String?,
+             var age: Int)
+
+class Empty
+
+fun main() {
+    val filip = Person("Filip", "Babic", 23)
+    println("Name: ${filip.name}, last name: ${filip.lastName}, Age: ${filip.age}")
+
+    val marin = Person("Marin", null, 24)
+    println("Name: ${marin.name}, last name: ${marin.lastName}, Age: ${marin.age}")
+
+    val filipClone = filip
+
+    filipClone.age = 24
+    println(filip.age)
+    println(filipClone.age)
+
+    val filipTwo = Person("Filip", "Babic", 24)
+
+    println(filip == marin)
+    println(filip == filipClone)
+    println(filip === filipClone)
+    println(filipTwo === filip)
 }
+
 
 
 
