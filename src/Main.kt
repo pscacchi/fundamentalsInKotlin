a3661d04923ef2e0dcdb4076351496e8327b08b0
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-30 21:56:31 -0300

    RC-70. 43. CC. Define properties, methods & class mutability

--- src/Main.kt
+++ src/Main.kt
@@ -2,45 +2,59 @@
 * Programming in Kotlin. Fundamentals
 * Part 06. Create classes
 *
-*  42. Model & create class
+*  43. Define properties, methods & class mutability
 */
 
 /*
+* CLASS CONCEPTS
 *
-*
+* Encapsulation
+*   Is the process when you abstract away information
+*   to hide away some key implementation details.
+* Mutability
+* Composition
+*   To expand the behavior of a class with another object
+*   Without adding multiple properties or methods
+* Reference & values
 * */
 
-//class Person {
-//    var name: String = ""
-//    var lastName: String? = null
-//    var age: Int = 0
-//}
+class Pet(var name: String, val animalType: String)
 
-class Person(val name: String,
-             val lastName: String?,
-             var age: Int)
 
-class Empty
+class Person (
+    private val name: String,
+    private val lastName: String?,
+    private var age: Int,
+    val pet: Pet
+) {
+    fun setAge(age: Int) {
+        if (age in 0..150) {
+            this.age = age
+        } else {
+            // invalid age
+        }
+    }
+
+    fun getAge() = age
+
+    fun getFullInformation(): String {
+        return "Full Name: $ lastName, $name, Age: $age, Pet: ${pet.name}, ${pet.animalType}"
+    }
+}
 
 fun main() {
-    val filip = Person("Filip", "Babic", 23)
-    println("Name: ${filip.name}, last name: ${filip.lastName}, Age: ${filip.age}")
+    var dog = Pet("Jura", "Goldie/Husky/Labrador Mix!")
+    val filip = Person("Filip", "Babic", 23, dog)
+    println(filip.getFullInformation())
 
-    val marin = Person("Marin", null, 24)
-    println("Name: ${marin.name}, last name: ${marin.lastName}, Age: ${marin.age}")
+    filip.setAge(75)
+    println(filip.getAge())
 
-    val filipClone = filip
+    dog.name = "Scratchy"
 
-    filipClone.age = 24
-    println(filip.age)
-    println(filipClone.age)
+    dog = Pet("Jura", "Goldie")
 
-    val filipTwo = Person("Filip", "Babic", 24)
-
-    println(filip == marin)
-    println(filip == filipClone)
-    println(filip === filipClone)
-    println(filipTwo === filip)
+    println(filip.pet.name)
 }
 
 
