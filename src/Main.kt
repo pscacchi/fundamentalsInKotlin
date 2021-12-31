e0d80ff934e876b0eb9a0e93e319fc964667ba6c
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-30 23:39:36 -0300

    RC-71. 44. CC. Challenge class

--- src/Main.kt
+++ src/Main.kt
@@ -2,59 +2,39 @@
 * Programming in Kotlin. Fundamentals
 * Part 06. Create classes
 *
-*  43. Define properties, methods & class mutability
+*  44. Challenger: Class
 */
 
 /*
-* CLASS CONCEPTS
+* CHALLENGE
 *
-* Encapsulation
-*   Is the process when you abstract away information
-*   to hide away some key implementation details.
-* Mutability
-* Composition
-*   To expand the behavior of a class with another object
-*   Without adding multiple properties or methods
-* Reference & values
+* Create a class which represents a Movie,
+* a Video Game or a Song. Add appropriate
+* properties like "name", "genre", "length"
+*
+* Add methods which format the properties
+* in a String for you to print out.
+*
+* Create a few objects of the class type &
+* print out their data.
+*
 * */
 
-class Pet(var name: String, val animalType: String)
-
-
-class Person (
+class ArtWork(
     private val name: String,
-    private val lastName: String?,
-    private var age: Int,
-    val pet: Pet
+    private val genre: String,
+    private val length: Int,
+    private val type: String
 ) {
-    fun setAge(age: Int) {
-        if (age in 0..150) {
-            this.age = age
-        } else {
-            // invalid age
-        }
-    }
-
-    fun getAge() = age
-
-    fun getFullInformation(): String {
-        return "Full Name: $ lastName, $name, Age: $age, Pet: ${pet.name}, ${pet.animalType}"
-    }
+    fun getResume() = "$name is a $type del genre $genre with a length of $length seconds"
 }
 
 fun main() {
-    var dog = Pet("Jura", "Goldie/Husky/Labrador Mix!")
-    val filip = Person("Filip", "Babic", 23, dog)
-    println(filip.getFullInformation())
+    val movie = ArtWork("Ninja attack", "action", 98 * 60,"movie")
+    val song = ArtWork("I fell good", "rock", 210, "song")
 
-    filip.setAge(75)
-    println(filip.getAge())
-
-    dog.name = "Scratchy"
-
-    dog = Pet("Jura", "Goldie")
-
-    println(filip.pet.name)
+    println(movie.getResume())
+    println(song.getResume())
 }
 
 
