c2af7e897ee979ed4faf3abc480d921b7d838bcc
PScacchi <pabloantonio-scacchi@endava.com>
2021-12-31 00:31:42 -0300

    RC-74. 47. CC. Conclusion

--- src/Main.kt
+++ src/Main.kt
@@ -2,52 +2,11 @@
 * Programming in Kotlin. Fundamentals
 * Part 06. Create classes
 *
-*  46. Challenge. Data class
+*  47. Conclusion
+*
+* It's all. Folk
 */
-
-/*
-* CHALLENGE
-*
-* Change the class from the first
-* challenge to be a 'data' class
-* instead & make sure the class is immutable.
-*
-* Create a copy of a certain object
-* you created, and compare it for data
-* equality and identity (reference equality.
-*
-* Destructure the object, and use the properties
-* to print out its data.
-*/
-
-
-data class ArtWork(
-    val name: String,
-    val genre: String,
-    val length: Int,
-    val type: String
-) {
-    fun getResume() = "$name is a $type del genre $genre with a length of $length seconds"
-}
 
 fun main() {
-    val movie = ArtWork("Ninja attack", "action", 98 * 60,"movie")
-    val song = ArtWork("I fell good", "rock", 210, "song")
 
-    println(movie.getResume())
-    println(song.getResume())
-
-    val movieTwo = movie.copy()
-    println(movie == movieTwo)
-    println(movie === movieTwo)
-
-    val (name, genre, length, type) = movie
-
-    println("$name, $type, $genre, $length")
 }
-
-
-
-
-
-
